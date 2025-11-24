c3193e1e05a71409dc215116abbdb879199e2ddf
Deimos Laboratories <github_@deimos.ca>
2025-11-23 23:29:01 -0500

    added GPL (COPYING and source headers)

--- dirt.h
+++ dirt.h
@@ -2,6 +2,20 @@
 /* DIRT - Delt's Impulse Response Tool
  * Version 0.1
  *
+ * -----------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------------
  * A simple, open-source sinewave sweep generator, recorder, and  deconvolver.
  * Takes two sinewave sweeps, one generated (dry) and the other (wet) recorded
  * through a cab, reverb, etc. and outputs an impulse response.
@@ -17,6 +31,7 @@
  * dirt <out_jack_port> <in_jack_port> -o <output_ir.wav>
  *
  * See --help text for more info/options
+ *
  */
  
 #ifndef __DECONVOLVER_H
