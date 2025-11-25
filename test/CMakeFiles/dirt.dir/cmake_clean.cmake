fcd4f9a25cbc1c5a9417b358fb4fa28262eca8f6
Deimos Laboratories <github_@deimos.ca>
2025-11-25 14:23:42 -0500

    COPYING, test wav files

---
+++ test/CMakeFiles/dirt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dirt.dir/dirt.cpp.o"
+  "CMakeFiles/dirt.dir/dirt.cpp.o.d"
+  "dirt"
+  "dirt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dirt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
