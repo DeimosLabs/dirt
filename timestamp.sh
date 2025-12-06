5149daff74f74c3622fa0db773d6770248578fe7
Deimos Laboratories <github_@deimos.ca>
2025-12-06 07:59:23 -0500

    Fixed timestamp script

--- timestamp.sh
+++ timestamp.sh
@@ -2,8 +2,10 @@
 tmpfile="$1-new"
 outfile="$1"
 
+echo "outputting to $1"
+
 echo "#define BUILD_TIMESTAMP \"$(date +%Y%m%d_%H%M%S)\"" > "$tmpfile"
-if diff "$tmpfile" "$outfile" 2>/dev/null || ! [ -f "$tmpfile" ]; then
+if diff "$tmpfile" "$outfile" 2>/dev/null || ! [ -f "$outfile" ]; then
   mv -f "$tmpfile" "$outfile"
 else
   rm -f "$tmpfile"
